115929ffa4f97153cf0ccdf47ce31710d919f171
samfan01 <dsamfan@gmail.com>
2021-04-13 00:49:15 +0300

    Created the profile class that helps create new instances of profile

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    name = models.CharField(Max_length = 60)
+    email = models.EmailField()
+    neighbouhood_id = models.ForeignKey('NeighbourHood',on_delete=models.CASCADE,related_name = 'profile')
