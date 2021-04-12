d97f53ad0483010031419bea9b28229b884f5143
samfan01 <dsamfan@gmail.com>
2021-04-12 09:56:35 +0300

    created the urls.py file to hold my app routes

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    title = 'Welcome to the Neighbourhood'
+    template_name = 'home.html'
+    
+    return render(request,template_name,{'title':title})
