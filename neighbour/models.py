6cc06cba26cc1522d198ec9283132f24e4b446bb
samfan01 <dsamfan@gmail.com>
2021-04-13 01:24:29 +0300

    created the Neighbourhood model class

--- neighbour/models.py
+++ neighbour/models.py
@@ -6,6 +6,16 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    neighbouhood_name = models.CharField(Max_length = 90)
+    neighbouhood_location = models.CharField(Max_length = 90)
+    occupants = models.CharField(Max_length = 90)
+
+    def __str__(self):
+        return self.neighbouhood_name
+    
+    
+        
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
