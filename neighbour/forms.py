6cc620ca7c22c14f5cba3c0c19b19771c71457b5
samfan01 <dsamfan@gmail.com>
2021-04-13 03:38:39 +0300

    created the Profile form to help users edit their profiles

--- neighbour/forms.py
+++ neighbour/forms.py
@@ -1,1 +1,14 @@
 from django import forms
+from . models import *
+
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('email','name')
+        
+        widgets = {
+            'email': forms.TextInput(attrs = {'class': 'form-control'}),
+            'name': forms.TextaInput(attrs = {'class': 'form-control'}),
+        }
