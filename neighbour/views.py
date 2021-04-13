a3185c556b413ae2437d24ee437859ba2b714ff2
samfan01 <dsamfan@gmail.com>
2021-04-13 03:17:43 +0300

    added a bootstrap card to display some neighbourhoods on the homepage

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
     title = 'Welcome to the Neighbourhood'
     template_name = 'home.html'
+    neighbourhoods = NeighbourHood.objects.all()
     
-    return render(request,template_name,{'title':title})
+    return render(request,template_name,{'title':title,'neghbourhoods':neighbourhoods})
