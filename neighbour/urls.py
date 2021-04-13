4d3be0858b46bb886a2eb7f03fbfef7b9d86622b
samfan01 <dsamfan@gmail.com>
2021-04-13 03:48:58 +0300

    created the profile.html that will hold and display user profile information

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns=[
-    path('',views.home,name = 'home')
+    path('',views.home,name = 'home'),
     path('profile',views.profile,name = 'profile'),
 ]
 
